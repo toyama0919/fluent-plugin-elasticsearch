6d60c3b74fee6122b8dc2f3f59ea0927b68c4ff7
Hiroshi Toyama <toyama-h@ipros.jp>
2014-08-28 13:09:38 +0900

    add generate id option format rubocop

--- lib/fluent/plugin/out_elasticsearch.rb
+++ lib/fluent/plugin/out_elasticsearch.rb
@@ -5,16 +5,16 @@ require 'date'
 class Fluent::ElasticsearchOutput < Fluent::BufferedOutput
   Fluent::Plugin.register_output('elasticsearch', self)
 
-  config_param :host, :string,  :default => 'localhost'
-  config_param :port, :integer, :default => 9200
-  config_param :logstash_format, :bool, :default => false
-  config_param :logstash_prefix, :string, :default => "logstash"
-  config_param :logstash_dateformat, :string, :default => "%Y.%m.%d"
-  config_param :type_name, :string, :default => "fluentd"
-  config_param :index_name, :string, :default => "fluentd"
-  config_param :id_key, :string, :default => nil
-  config_param :add_timestamp, :string, :default => false
-  config_param :timestamp_key, :string, :default => "@timestamp"
+  config_param :host, :string,  default: 'localhost'
+  config_param :port, :integer, default: 9200
+  config_param :logstash_format, :bool, default: false
+  config_param :logstash_prefix, :string, default: 'logstash'
+  config_param :logstash_dateformat, :string, default: '%Y.%m.%d'
+  config_param :type_name, :string, default: 'fluentd'
+  config_param :index_name, :string, default: 'fluentd'
+  config_param :id_keys, :string, default: nil
+  config_param :add_timestamp, :string, default: false
+  config_param :timestamp_key, :string, default: '@timestamp'
 
   include Fluent::SetTagKeyMixin
   config_set_default :include_tag_key, false
@@ -25,6 +25,10 @@ class Fluent::ElasticsearchOutput < Fluent::BufferedOutput
 
   def configure(conf)
     super
+    if @id_keys
+      @id_keys = @id_keys.split(',')
+      @id_format = @id_keys.map { |_key| '%s' }.join('_')
+    end
   end
 
   def start
@@ -45,7 +49,7 @@ class Fluent::ElasticsearchOutput < Fluent::BufferedOutput
     chunk.msgpack_each do |tag, time, record|
 
       if @add_timestamp
-        record.merge!({@timestamp_key => Time.at(time).to_datetime.to_s})
+        record.merge!(@timestamp_key => Time.at(time).to_datetime.to_s)
       end
 
       if @logstash_format
@@ -58,18 +62,22 @@ class Fluent::ElasticsearchOutput < Fluent::BufferedOutput
         record.merge!(@tag_key => tag)
       end
 
-      meta = { "index" => {"_index" => target_index, "_type" => type_name} }
-      if @id_key && record[@id_key]
-        meta['index']['_id'] = record[@id_key]
-      end
+      meta = { 'index' => { '_index' => target_index, '_type' => type_name } }
+      meta['index']['_id'] = generate_id(record) if @id_keys
       bulk_message << Yajl::Encoder.encode(meta)
       bulk_message << Yajl::Encoder.encode(record)
     end
-    bulk_message << ""
+    bulk_message << ''
 
     http = Net::HTTP.new(@host, @port.to_i)
-    request = Net::HTTP::Post.new('/_bulk', {'content-type' => 'application/json; charset=utf-8'})
+    request = Net::HTTP::Post.new('/_bulk', 'content-type' => 'application/json; charset=utf-8')
     request.body = bulk_message.join("\n")
     http.request(request).value
+  end
+
+  private
+
+  def generate_id(record)
+    @id_format % @id_keys.map { |key| record[key] }
   end
 end
